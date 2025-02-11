662b41d57447c38d1edb53f4a352c05bcf89a426
John Willson <johnwillson.edwinsamuvel@grandparade.co.uk>
2025-02-11 18:18:16 +0100

    removed yaml files

--- src/main/java/com/kanezi/springdoc_openapi_showcase/EmailApi.java
+++ src/main/java/com/kanezi/springdoc_openapi_showcase/EmailApi.java
@@ -5,12 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
-// @Tag(name = "email", description = "email API")
 public interface EmailApi {
 
-    // tag::validation-openapi[]
     record EmailRequest(String to, String subject,String text) {}
-    // end::validation-openapi[]
 
     record EmailResponse(String id, String message) {}
 
@@ -23,7 +20,5 @@ public interface EmailApi {
     ResponseEntity<String> checkStatus(
             String id);
 
-    // tag::spring-data-jpa-openapi-supported[]
     Page<Object> filterEmails(Pageable pageable);
-    // end::spring-data-jpa-openapi-supported[]
 }
